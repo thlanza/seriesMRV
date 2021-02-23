ae88338dac636d39ea6f4c99c9bb2612a5b3aed0
Thiago Lanza <thlanza@hotmail.com>
2021-02-22 21:29:17 -0300

    first

---
+++ Classes/SerieRepositorio.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using Series.Interfaces;
+
+namespace Series.Classes
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+        private List<Serie> listaSerie = new List<Serie>();
+
+        public void Atualiza(int id, Serie entidade)
+        {
+            listaSerie[id] = entidade;
+        }
+
+        public void Exclui(int id)
+        {
+            listaSerie[id].Excluir();
+        }
+
+        public void Insere(Serie entidade)
+        {
+            listaSerie.Add(entidade);
+        }
+
+        public List<Serie> Lista()
+        {
+            return listaSerie;
+        }
+
+        public int ProximoId()
+        {
+            return listaSerie.Count;
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            return listaSerie[id];
+        }
+    }
+}
